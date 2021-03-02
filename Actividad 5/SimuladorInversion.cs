5e83b71ccae8613b2f502b0d252ad9330ab5ae72
Esmeralda Estrada <esmeraldamaribel115@outlook.com>
2021-03-01 22:20:55 -0600

    actividad 5

---
+++ Actividad 5/SimuladorInversion.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actividad_5
+{
+   public class SimuladorInversion:INotifyPropertyChanged
+    {
+        private decimal monto;
+
+        private short plazo;
+
+
+        public decimal MontoInvertir
+        {
+            get { return monto; }
+            set { monto = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("")); }
+
+        }
+
+        public short PlazoAnual
+        {
+            get { return plazo; }
+            set { plazo = value; }
+        }
+
+        public decimal Total
+        {
+            get
+            {
+
+                decimal CantidadTotal = monto;
+
+                for (int i = 0; i != plazo; i++)
+
+                {
+
+                    CantidadTotal += (CantidadTotal * 0.07m);
+
+                    CantidadTotal = (CantidadTotal > 30000) ? CantidadTotal - (CantidadTotal * 0.02m) : CantidadTotal;
+
+                }
+
+                return CantidadTotal;
+               
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
